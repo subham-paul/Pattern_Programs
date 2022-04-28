fab7376c951d26a9d40ab8d70e67cbada45acceb
Subham Paul <subhampaul456@gmail.com>
2022-04-28 22:04:38 +0530

    Pattern in Java

---
+++ Pattern_1.java
@@ -0,0 +1,18 @@
+/* 
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * * 
+*/
+
+public class Pattern_1 {
+    public static void main(String args[]) {
+        for (int row = 0; row < 5; row++) {
+            for (int col = 0; col < 5; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
