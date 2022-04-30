261e5184c59f13657478e22cf33a62386d224c48
Subham Paul <subhampaul456@gmail.com>
2022-04-30 23:34:36 +0530

    Pattern 3 solution

---
+++ Pattern_3.java
@@ -0,0 +1,17 @@
+/* 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5 
+1 2 3 4 5  
+*/
+public class Pattern_3 {
+    public static void main(String args[]) {
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
+    }
+}
