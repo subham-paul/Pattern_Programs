42e28be04e8255342dd7cbed5136d41d0ff75910
Subham Paul <subhampaul456@gmail.com>
2022-05-07 00:08:04 +0530

    pattern 7

--- Pattern_7.java
+++ Pattern_7.java
@@ -1,3 +1,19 @@
-public class enum Pattern_7 {
-    
+/* 
+    1 3 5 7 9
+    11 13 15 17 19
+    21 23 25 27 29
+    31 33 35 37 39
+    41 43 45 47 49
+ */
+public class Pattern_7 {
+    public static void main(String args[]) {
+        int count = 1;
+        for (int row = 1; row <= 5; row++) {
+            for (int col = 1; col <= 5; col++) {
+                System.out.print(" " + count);
+                count = count + 2;
+            }
+            System.out.println();
+        }
+    }
 }
