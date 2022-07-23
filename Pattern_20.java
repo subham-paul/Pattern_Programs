732c1a51cabdfae7530fbd799ea7af94e4c4e907
Subham Paul <subhampaul456@gmail.com>
2022-07-23 10:12:40 +0530

    Pattern 20

--- Pattern_20.java
+++ Pattern_20.java
@@ -1,18 +1,20 @@
-/*
+
+/* 
+1 0 1 0 1 
+0 0 0 0 0
 1 0 1 0 1
-0 1 0 1 0
-1 0 1 0 1
-0 1 0 1 0
+0 0 0 0 0 
 1 0 1 0 1 */
 import java.util.Scanner;
-public class Pattern_19 {
+
+public class Pattern_20 {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Hit a number for print -> ");
         int userIp = sc.nextInt();
         for (int row = 1; row <= userIp; row++) {
-            for (int col = 0; col < userIp; col++) {
-                System.out.print((row + col) % 2 + " ");
+            for (int col = 1; col <= userIp; col++) {
+                System.out.print((row * col) % 2 + " ");
             }
             System.out.println();
         }
