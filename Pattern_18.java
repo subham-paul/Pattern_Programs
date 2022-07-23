3dc92e0a2395d9ca52fca4c1dc3bcf2339ca3c87
Subham Paul <subhampaul456@gmail.com>
2022-07-23 09:38:29 +0530

    Pattern-18

---
+++ Pattern_18.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+/* 
+0 1 0 1 0
+1 0 1 0 1
+0 1 0 1 0
+1 0 1 0 1
+0 1 0 1 0 */
+
+public class Pattern_18 {
+    public static void main(String args[]) {
+        Scanner subham = new Scanner(System.in);
+        System.out.print("Enter a range for print pattern-> ");
+        int n = subham.nextInt();
+        // int n = 5;
+        for (int row = 1; row <= n; row++){
+            for (int col = 1; col <= n; col++) {
+                System.out.print((row+col)%2 + " ");
+            }
+            System.out.println();
+        }
+    }
+}
