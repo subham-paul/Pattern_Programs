8db10ee5f7919100d5480a1c1f452afcb4d67867
Subham <subhampaul456@gmail.com>
2022-08-30 23:22:07 +0530

    Pattern 26

---
+++ Pattern_26.java
@@ -0,0 +1,10 @@
+public class Pattern_26 {
+    public static void main(String args[]) {
+        for (char row = 'A'; row <= 'E'; row++) {
+            for (char col = 'A'; col <= 'E'; col++) {
+                System.out.print(row + " ");
+            }
+            System.out.println();
+        }
+    }
+}
