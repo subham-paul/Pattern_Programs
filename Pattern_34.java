2c8989df84077a6a5918b94a61f84c6e99a101f1
Subham <subhampaul456@gmail.com>
2023-01-31 18:05:28 +0530

    wip

--- Pattern_34.java
+++ Pattern_34.java
@@ -6,8 +6,8 @@ public class Pattern_34 {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter number here -> ");
         n = sc.nextInt();
-        for (int row = 0; row <= n; row++) {
-            for (int col = 0; col < row; col++) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
                 System.out.print("* ");
             }
             System.out.println();
