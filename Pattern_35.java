1d665b7105f540dbbdf0d41cb6dbce6ef46e30f9
Subham <subhampaul456@gmail.com>
2023-08-01 20:56:20 +0530

    new pattern added

--- Pattern_35.java
+++ Pattern_35.java
@@ -1,22 +1,20 @@
 
-/* 
-Enter number here -> 5
-1
+/* Enter a number here -> 5
+1 
 2 2
 3 3 3
 4 4 4 4
-5 5 5 5 5
- */
+5 5 5 5 5 */
 import java.util.Scanner;
 
-public class Pattern_34 {
+public class Pattern_35 {
     public static void main(String args[]) {
         int n;
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter number here -> ");
+        System.out.print("Enter a number here -> ");
         n = sc.nextInt();
-        for (int row = 1; row <= n; row++) {
-            for (int col = 1; col <= row; col++) {
+        for (int row = 1; row <= n; ++row) {
+            for (int col = 1; col <= row; ++col) {
                 System.out.print(row + " ");
             }
             System.out.println();
