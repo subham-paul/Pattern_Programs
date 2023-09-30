57275207f43341b775155cd3fce9cad835e24f8b
Subham <subhampaul456@gmail.com>
2023-09-30 18:02:25 +0530

    done

--- Pattern_117.java
+++ Pattern_117.java
@@ -1,15 +1,15 @@
 /* 
-    *
-   **
-  ***
- ****
-***** */
+****
+ ***
+  **
+   *
+ */
 
-public class Pattern_95 {
-    public static void main(String agrs[]) {
+public class Pattern_117 {
+    public static void main(String args[]) {
         int n = 5;
-        for (int row = 1; row <= n; row++) {
-            for (int col = n - 1; col >= row; col--) {
+        for (int row = n; row >= 1; row--) {
+            for (int space = n - 1; space >= row; space--) {
                 System.out.print(" ");
             }
             for (int col = 1; col <= row; col++) {
