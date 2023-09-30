2867b77ba54c783b1070f3576546843ef910cd92
Subham <subhampaul456@gmail.com>
2023-09-30 11:33:31 +0530

    Pattern_70 Done

--- Pattern_70.java
+++ Pattern_70.java
@@ -8,5 +8,13 @@
  */
 
 public class Pattern_70 {
-    
+    public static void main(String args[]) {
+        int n = 5;
+        for (int row = 1; row < n; row++) {
+            for (int col = n; col >= row; col--) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
 }
