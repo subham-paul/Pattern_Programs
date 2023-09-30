0024b06583a861672e1707931aaa234d970198d3
Subham <subhampaul456@gmail.com>
2023-09-30 18:49:41 +0530

    pattern solve

--- Pattern_117.java
+++ Pattern_117.java
@@ -9,9 +9,11 @@ public class Pattern_117 {
     public static void main(String args[]) {
         int n = 5;
         for (int row = n; row >= 1; row--) {
+
             for (int space = n - 1; space >= row; space--) {
                 System.out.print(" ");
             }
+
             for (int col = 1; col <= row; col++) {
                 System.out.print("*");
             }
---
+++ Pattern_133.java
@@ -0,0 +1,22 @@
+/* 
+* * * * * 
+ * * * * 
+  * * * 
+   * * 
+    * 
+ */
+public class Pattern_133 {
+    public static void main(String args[]){
+        int n = 5;
+        for (int row = n; row >= 1; row--) {
+            for (int col = n; col >= 1; col--) {
+                if (row >= col) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
