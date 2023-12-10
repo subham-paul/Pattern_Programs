24b3851828af8a4e0c8f3041a9a79228f85dec9e
Subham <subhampaul456@gmail.com>
2023-12-10 11:10:50 +0530

    Done

--- P1_SqHollowPattern.java
+++ P1_SqHollowPattern.java
@@ -1,5 +1,23 @@
+/* 
+* * * * * 
+*       *
+*       *
+*       *
+*       *
+* * * * *
+ */
 public class P1_SqHollowPattern {
-    public static void main(String args[]){
-        
+    public static void main(String args[]) {
+        int n = 6;
+        for (int row = 0; row < n; row++) {
+            for (int col = 0; col < n - 1; col++) {
+                if (row == 0 || row == n - 1 || col == 0 || col == n - 2) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
