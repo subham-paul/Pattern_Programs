ed444b04c5f99ccf08ce0b43b976cc1695f6a61a
Subham <subhampaul456@gmail.com>
2023-12-13 21:51:46 +0530

    done

--- P3_No_increasing_Pyramid.java
+++ P3_No_increasing_Pyramid.java
@@ -1,11 +1,19 @@
+/* 
+Print a number -> 5
+1 
+1 2 
+1 2 3 
+1 2 3 4 
+1 2 3 4 5
+ */
 import java.util.Scanner;
 
 public class P3_No_increasing_Pyramid {
     public static void main(String args[]) {
-        // int n = 5;
-        Scanner sc=new Scanner(System.in);
-        System.out.print("Print a number -> ");
-        int n=sc.nextInt();
+        int n = 5;
+        // Scanner sc=new Scanner(System.in);
+        // System.out.print("Print a number -> ");
+        // int n=sc.nextInt();
         for (int row = 1; row <= n; row++) {
             for (int col = 1; col <= row; col++) {
                 System.out.print(col + " ");
--- P4_No_IncreasingReversePyramid.java
+++ P4_No_IncreasingReversePyramid.java
@@ -1,11 +1,24 @@
+
+/* 
+1 2 3 4 5 
+1 2 3 4
+1 2 3
+1 2
+1
+ */
 import java.util.Scanner;
 
 public class P4_No_IncreasingReversePyramid {
-    public static void main(String args[]){
-        Scanner sc =new Scanner(System.in);
-        System.out.print("ENTER A NUMBER ->");
-        int n=sc.nextInt();
-        
-
+    public static void main(String args[]) {
+        int n = 5;
+        // Scanner sc = new Scanner(System.in);
+        // System.out.print("ENTER A NUMBER -> ");
+        // int n = sc.nextInt();
+        for (int row = n; row >= 1; row--) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
     }
 }
