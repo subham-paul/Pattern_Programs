eda7aea51fe0075113b6375181e501e96820feb5
subham-paul <subhampaul456@gmail.com>
2023-12-13 17:35:10 +0530

    done

--- P3_No_increasing_Pyramid.java
+++ P3_No_increasing_Pyramid.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class P3_No_increasing_Pyramid {
-    public static void main(String args[]){
-        int n=5
+    public static void main(String args[]) {
+        // int n = 5;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Print a number -> ");
+        int n=sc.nextInt();
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print(col + " ");
+            }
+            System.out.println();
+        }
     }
 }
