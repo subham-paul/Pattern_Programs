eca662b62a1650c631d6feb6bb206b098f6325e5
subham-paul <subhampaul456@gmail.com>
2023-12-13 18:03:02 +0530

    wip

---
+++ P4_No_IncreasingReversePyramid.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class P4_No_IncreasingReversePyramid {
+    public static void main(String args[]){
+        Scanner sc =new Scanner(System.in);
+        System.out.print("ENTER A NUMBER ->");
+        int n=sc.nextInt();
+        
+
+    }
+}
