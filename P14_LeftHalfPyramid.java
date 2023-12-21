b46bbe940d335b5ff041f9d0cb2e08f05aebfdfb
subham-paul <subhampaul456@gmail.com>
2023-12-21 11:42:13 +0530

    Done

---
+++ P14_LeftHalfPyramid.java
@@ -0,0 +1,34 @@
+
+/* 
+    *
+   **
+  ***
+ ****
+ */
+import java.util.Scanner;
+
+public class P14_LeftHalfPyramid {
+    public static void printPattern(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = n; col > row; col--) {
+
+                System.out.print(" ");
+            }
+            for (int col = 1; col <= row; col++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String args[]) {
+        /*
+         * Scanner sc = new Scanner(System.in);
+         * System.out.print("Enter a number -> ");
+         * int n = sc.nextInt();
+         */
+        int n = 5;
+        printPattern(n);
+    }
+
+}
