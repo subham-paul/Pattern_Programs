6560bc01eb54b806ea6fd9b19459f5668866aaf7
Subham <subhampaul456@gmail.com>
2024-01-16 00:33:48 +0530

    hollowstar print

---
+++ JS_3_HollowStar.js
@@ -0,0 +1,15 @@
+const n=5;
+let str="";
+
+for(let row=0;row<n;row++){
+    for(let col=0;col<n;col++){
+        if(row==0 || row==n-1 ||col==0 || col==n-1){
+            str += "*";
+        } else {
+            str += " ";
+        }
+    }
+    str += "\n";
+}
+
+console.log(str);
